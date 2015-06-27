6f6fa45d74738e4279d2fb672756dae02cf3e1a1
umiyosh <yumino99@gmail.com>
2015-06-27 18:19:48 +0900

    add finder setting

--- bin/misc.sh
+++ bin/misc.sh
@@ -8,6 +8,11 @@ defaults write com.apple.dock springboard-hide-duration -int 0
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool NO; killall Dock
 defaults write com.apple.dock workspaces-edge-delay -float 0;killall Dock
 
+# finder settings
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+defaults write com.apple.Finder AppleShowAllFiles YES
+killall Finder
+
 # plist restore
 if [[ ! -e $HOME/Dropbox/Mackup/.mackup.cfg ]]; then
   ln -s %HOME/dotfiles/.mackup.cfg $HOME/Dropbox/Mackup/.mackup.cfg
